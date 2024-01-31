151b97ee1a442dfac32859987acbd78814ef25a4
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-01-31 14:58:09 +0330

    types in ts

--- build/test.js
+++ build/test.js
@@ -1,6 +1,28 @@
 "use strict";
-var m = "tets";
-function aw(bb) {
-    return bb;
+// var m: string = "tets";
+// function aw(bb: string) {
+//   return bb;
+// }
+// console.log(aw(m));
+// const data: {
+//   name: string;
+//   age: number;
+// } = {
+//   name: "amir hossein",
+//   age: 26,
+// };
+// console.log(data.name);
+var Personal;
+(function (Personal) {
+    Personal["NAME"] = "amir hossein";
+    Personal[Personal["MALE"] = 0] = "MALE";
+    Personal[Personal["FEMALE"] = 1] = "FEMALE";
+})(Personal || (Personal = {}));
+const obj = {
+    name: Personal.NAME,
+    age: 26,
+    gender: Personal.MALE,
+};
+if (obj.gender === Personal.MALE) {
+    console.log("he is Male");
 }
-console.log(aw(m));
