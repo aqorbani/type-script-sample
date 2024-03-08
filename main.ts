215b1932a349661d7920db187edca8867d393b10
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-03-08 18:39:42 +0330

    function type

--- main.ts
+++ main.ts
@@ -2,12 +2,19 @@
 
 // cartItems = ["", "", "", 2, 3, 6];
 
-function addProduct(title: string, price: number): void {
-  const product = title + price;
-  console.log(product);
-}
+// function addProduct(title: string, price: number): void {
+//   const product = title + price;
+//   console.log(product);
+// }
 
-function add(num1: number, num2: number): number {
-  const result = num1 + num2;
-  return result;
+// function add(num1: number, num2: number): number {
+//   const result = num1 + num2;
+//   return result;
+// }
+function createUser(
+  id: number,
+  email: string,
+  role: (userId: number, isAdmin: boolean) => void
+) {
+  role(3, true);
 }
