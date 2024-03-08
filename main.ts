f604cc837408cf01693bb29427d4fc67972e4d78
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-03-08 19:06:42 +0330

    interface

--- main.ts
+++ main.ts
@@ -11,10 +11,42 @@
 //   const result = num1 + num2;
 //   return result;
 // }
-function createUser(
-  id: number,
-  email: string,
-  role: (userId: number, isAdmin: boolean) => void
-) {
-  role(3, true);
+
+// function createUser(
+//   id: number,
+//   email: string,
+//   role: (userId: number, isAdmin: boolean) => void
+// ) {
+//   role(3, true);
+// }
+
+// type FnType = (userId: number, isAdmin: boolean) => void;
+
+// function createUser(id: number, email: string, role: FnType) {
+//   role(3, true);
+// }
+
+interface User {
+  email: string;
+  password: number;
 }
+interface User {
+  id: number;
+}
+
+let user: User = {
+  id: 1,
+  email: "a@abstract.com",
+  password: 123,
+};
+
+class Auth implements User {
+  id: number;
+  email: string;
+  password: number;
+  name: string;
+}
+
+function login(userInfo: User) {}
+
+login(new Auth());
